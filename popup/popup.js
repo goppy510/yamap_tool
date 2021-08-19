44761e5827bc6417b1aaa99dc4b7f8f22acee3be
unknown <tacumin.goppy.06@vesta.ocn.ne.jp>
2021-08-19 10:29:43 +0900

    URLのマッチングをtestに変更、及びMV形式に合わせて一部項目取得)

--- popup/popup.js
+++ popup/popup.js
@@ -1,11 +1,9 @@
 window.onload = () => {
 
-  const ACTIVITY_URL = 'https://yamap.com/activities/';
-
   // 現在のタブのURLを取得して比較する
   chrome.tabs.getSelected( null, function(tab) {
     const tagExport = document.getElementsByClassName('export');
-    if ( tab.url.indexOf(ACTIVITY_URL) === 0 ) {
+    if ( /https:\/\/yamap.com\/activities\/[0-9].+\/article/.test(tab.url)) {
       // 活動日記のURLのとき
       createButton(tagExport);
     } else {
@@ -13,11 +11,43 @@ window.onload = () => {
     }
   } );
 
+  // // エクスポートボタンが押されたらhtmlを取得する
+  // const exportButton = document.getElementById('submit');
+  // exportButton.onclick = () => {
+  //   // 
+  //   chrome.storage.local.set( {
+  //     selected_html: 
+  //   })
+  // }
+
+  // 著者取得
+  function getAuthor() {
+    const author = document.getElementsByClassName('ActivityDetailTabLayout__UserName')[0].innerText;
+    return author;
+  }
+  // タイトルを取得
+  function getTitle() {
+    const title = document.getElementsByClassName('ActivityDetailTabLayout__Title')[0].innerText;
+    return title;
+  }
+
+  // 公開ならMV形式に合わせてPUBLICを、非公開ならDRAFTを返す
+  function getStatus() {
+    const publicType = document.getElementsByClassName("ActivityDetailTabLayout__Header__PublicType");
+    if (publicType === undefined) {
+      return 'PUBLIC';
+    }
+    return 'DRAFT';
+  }
+
+  // 
+
   // エクスポートボタンを作成するメソッド
   function createButton(element) {
     var btn = document.createElement('button');
     btn.type = 'button';
     btn.classList.add('export_inner');
+    btn.setAttribute('id', 'submit');
     const MESSAGE = '活動日記をエクスポート';
     createMessageNode(btn, MESSAGE);
     // 親ノードを複数形(getElements)で取得しているのでindexが必要
