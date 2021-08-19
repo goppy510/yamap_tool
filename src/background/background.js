d92c5fe9fa9742606f5f7ce8b59c8164bc7a62f8
goppy <tacumin.goppy.06@gmail.com>
2021-08-19 23:14:26 +0900

    コメントを取得するの諦めた

--- src/background/background.js
+++ src/background/background.js
@@ -1,25 +1,21 @@
-chrome.runtime.onMessage.addListener( function(message, sender, sendResponse) {
-    const url = message.url;
-    if (url !== undefined) {
-        const commentURL = url.replace('/article', '');
-        chrome.tabs.create( {
-            url: `${commentURL}/comments#post`
-        } );
-        sendResponse();
-        return true;
-    }
+// chrome.runtime.onMessage.addListener( function(message, sender, sendResponse) {
+//     const url = message.url;
+//     if (url !== undefined) {
+//         const commentURL = url.replace('/article', '');
+//         chrome.tabs.create( {
+//             url: `${commentURL}/comments#post`,
+//             active: false
+//         } );
+//         const commentable = isCommentable();
+//         console.log(commentable);
+//         sendResponse();
+//         return true;
+//     }
 
-    // コメント投稿の可否を取得しMV形式に合わせてreturnする
-    function isCommentable() {
-      // 別ページから取得できるのか？
-      chrome.tabs.getSelected( null, function(tab) {
-        if ( /https:\/\/yamap.com\/activities\/[0-9].+\/comments#post/.test(url) ) {
-          const commentForm = document.getElementsByClassName("CommentForm Comments__CommentForm")[0];
-          if (commentForm === undefined) {
-            return 0;
-          }
-        }
-        return 1;
-      } );
-    }
-} );
+//     // コメント投稿の可否を取得しMV形式に合わせてreturnする
+//     function isCommentable() {
+//       // 別ページから取得できるのか？
+//       const commentForm = document.getElementsByClassName("Comments__CommentForm")[0];
+//       return commentForm === undefined ? 0 : 1;
+//     }
+// } );
