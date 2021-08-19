a8a92916b92c2591f200606f7c2a067bce1d7f73
goppy <tacumin.goppy.06@gmail.com>
2021-08-19 17:36:25 +0900

    不要コード削除

--- src/background/background.js
+++ src/background/background.js
@@ -1,9 +1,1 @@
-const onClick = () => (_, tab) => chrome.tabs.sendMessage(tab.id, '');
 
-chrome.contextMenus.create({
-    title : '活動記録のエクスポート',
-    type : 'normal',
-    contexts : ['all'],
-    id: 'parent_id',
-    onclick : onClick()
-});
